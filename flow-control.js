58e5c278c5f64cd5eeaa35d97e73eafc48f539d0
Omar Al Hennawi <omar504969@gmail.com>
2021-01-11 21:13:54 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -25,7 +25,8 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
-  console.log('You are ' (age >= '13' && age <= '19' ? 'a teenager' : 'not a teenager')
+  console.log('You are ' (age >= '13' && age <= '19' ?
+  'a teenager' : 'not a teenager'));
 }
 
 function switchAge(age) {
