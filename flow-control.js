84460ae06e9c735a5f37306b28c9200648d0e91f
Omar Al Hennawi <omar504969@gmail.com>
2021-01-11 18:59:37 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -2,9 +2,8 @@ function basicTeenager(age) {
   if (age >= '13' && age <= '19'){
     return "You are a teenager!"
   }
-    return "undefined"
 }
-basicTeenager ('15')
+
 
 function teenager(age) {
 
