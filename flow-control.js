9f1b1e5c1039fb22730cabb839b1ae54b78951d8
Omar Al Hennawi <omar504969@gmail.com>
2021-01-11 21:56:07 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -30,20 +30,20 @@ function ternaryTeenager(age) {
 function switchAge(age) {
   switch(age) {
     case "13":
-      console.log("You are a teenager");
+      return"You are a teenager";
     case "14":
-      console.log("You are a teenager");
+      return"You are a teenager";
     case "15":
-      console.log("You are a teenager");
+      return"You are a teenager";
     case "16":
-      console.log("You are a teenager");
+      return"You are a teenager";
     case "17":
-      console.log("You are a teenager");
+      return"You are a teenager";
     case "18":
-      console.log("You are a teenager");
+      return"You are a teenager";
     case "19":
-      console.log("You are a teenager");
+      return"You are a teenager";
     default:
-      console.log("You have an age");
+      return"You have an age";
   }
 }
